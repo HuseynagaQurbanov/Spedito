a8594ad244acb2baee0dee87e7258b14b36f533e
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2021-09-24 23:38:44 +0400

    Fixed some front-end errors

--- Spedito/Admin/Controllers/DashboardController.cs
+++ Spedito/Admin/Controllers/DashboardController.cs
@@ -1,5 +1,7 @@
 ﻿using Admin.Filters;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using SpeditoReposity.Repositories.ShoppingReposities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +12,20 @@ namespace Admin.Controllers
     [TypeFilter(typeof(Auth))]
     public class DashboardController : Controller
     {
+        private SpeditoReposity.Models.Admin _admin => RouteData.Values["Admin"] as SpeditoReposity.Models.Admin;
+        private readonly IMapper _mapper;
+        private readonly IProductRepository _productRepository;
 
+        public DashboardController(IMapper mapper,
+                                 IProductRepository productRepository)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
         public IActionResult Index()
         {
+            ViewBag.Products = _productRepository.GetProducts();
+
             return View();
         }
     }
